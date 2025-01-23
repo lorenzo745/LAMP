ebef1f3d9b7f03b2f328ca91795bbf76d619fe69
lorenzo745 <lorenzo.zanirato6@gmail.com>
2025-01-23 08:24:12 +0000

    commit

--- mysql/scuola.sql
+++ mysql/scuola.sql
@@ -13,8 +13,9 @@ CREATE TABLE IF NOT EXISTS studenti (
   id INT AUTO_INCREMENT PRIMARY KEY,
   cognome VARCHAR(100) NOT NULL,
   data_nascita DATE NOT NULL,
-  corso_studi VARCHAR(100) NOT NULL,
-  capogruppo INT
+  corso_studi INT NULL,
+  capogruppo INT,
+  FOREIGN KEY (corso_studi) REFERENCES corsi(id)ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 --crea una tabella per le materie--
@@ -45,9 +46,12 @@ VALUES
 --aggiunge studenti a tabella studenti--
 INSERT INTO studenti (cognome, data_nascita,corso_studi,capogruppo)
 VALUES
-    ( 'Rossi', '1990-05-15',1,2),
+    ('Rossi', '1990-05-15',1,2),
     ('Verdi', '1988-12-07',3,1),
-    ('Bianchi', '1995-03-20',2,3);
+    ('Bianchi', '1995-03-20',2,3),
+    ('Marchetti', '1995-03-20',2,1),
+    ('Marini', '1995-03-20',2,1),
+    ('Mele', '1995-03-20',2,1);
 
 --aggiunge materie a tabella materie--
 INSERT INTO materie (nome)
@@ -63,7 +67,19 @@ VALUES
     (1, 1, 9.0,'2024-01-12', 'bravo'),
     (1, 2, 8.5,'2024-12-23', 'bravissimo'),
     (1, 3, 9.5,'2025-9-23', 'malino'),
-    (2, 1, 8.0,'2024-7-27', 'ottimo');
+    (2, 1, 8.0,'2024-7-27', 'ottimo'),
+    (2, 2, 7.5,'2024-3-29', 'mediocre'),
+    (2, 3, 9.0,'2025-9-23', 'ottimo'),
+    (3, 1, 9.0,'2024-1-12', 'bravo'),
+    (3, 2, 8.5,'2024-12-23', 'bravissimo'),
+    (3, 3, 9.5,'2025-9-23', 'ottimo'),
+    (4, 1, 8.0,'2024-7-27', 'ottimo'),
+    (4, 2, 7.5,'2024-3-29', 'mediocre'),
+    (4, 3, 9.0,'2025-9-23', 'ottimo'),
+    (5, 1, 8.0,'2024-7-27', 'ottimo'),
+    (5, 2, 7.5,'2024-3-29', 'mediocre'),
+    (5, 3, 9.0,'2025-9-23', 'ottimo');
+
 
 select s.cognome, v.voto
 from studenti s join valutazioni v
@@ -81,6 +97,25 @@ select s.cognome, s1.cognome
 from studenti s, studenti s1
 where s.capogruppo = s1.id;
 
+select s.cognome, v.voto, m.nome as materia, YEAR(CURDATE())-YEAR(s.data_nascita) as età
+from studenti s, valutazioni v, materie m
+where s.id = v.studente_id and v.materia_id = m.id and s.cognome like 'M%'
+order by cognome;
+
+select s.cognome, COUNT(v.voto) as conteggio_voti, AVG(v.voto) as media,MIN(v.voto) as voto_minimo, MAX(v.voto) as voto_massimo
+from studenti s, valutazioni v
+where s.id = v.studente_id and s.id = 3;
+
+select s.cognome 
+from studenti s 
+where YEAR(CURDATE())-YEAR(data_nascita)>=18;
+
+select s.cognome, v.voto
+from studenti s left join valutazioni v 
+    ON s.id = v.studente_id
+    where v.id is NULL;
+
+
 
 mysqldump -u root scuola > scuola_v1_05122024.sql
-mysql -u root scuola < scuola_v1_051224.sql 
+mysql -u root scuola < scuola_v1_05122024.sql
